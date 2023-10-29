2f391101606ff22c217a43a36a89677b83850e28
Rajesh Limbachiya <rajesandhya_143@yahoo.co.in>
2023-10-29 20:46:44 +0000

    MyFirstProject

---
+++ MyFirstClass.java
@@ -0,0 +1,17 @@
+package myfirstpackage;
+
+public class MyFirstClass {
+
+    public static void main (String[] args) {
+
+        System.out.println( "What is a software?");
+
+/*   A software is set of programme, which is designed to perform a task.
+     It guides the hardware what to do. */
+
+        System.out.println( "What is software testing?");
+
+        System.out.println( " What are the benefit of software testing?");
+
+    }
+}
